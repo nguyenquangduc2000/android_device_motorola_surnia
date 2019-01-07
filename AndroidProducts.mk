25a8d2d8b3827e1e264cb97847eb86a87042de00
Nguyen Quang Duc <nguyenquangduc2000@gmail.com>
2019-01-07 23:03:03 +0700

    AospExtended Init

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
-# Copyright (C) 2014-2016 The CyanogenMod Project
-#               2017 The LineageOS Project
+#
+# Copyright 2015 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,20 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-$(call inherit-product, device/motorola/surnia/full_surnia.mk)
-
-# Boot animation
-TARGET_BOOTANIMATION_HALF_RES := true
-TARGET_SCREEN_WIDTH := 540
-TARGET_SCREEN_HEIGHT := 960
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := surnia
-PRODUCT_NAME := lineage_surnia
-PRODUCT_BRAND := Motorola
-PRODUCT_MANUFACTURER := Motorola
-PRODUCT_RELEASE_NAME := surnia
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="Moto E LTE (2nd gen)"
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_surnia.mk
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo aosp_surnia-eng
+add_lunch_combo aosp_surnia-user
+add_lunch_combo aosp_surnia-userdebug
